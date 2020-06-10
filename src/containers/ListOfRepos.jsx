ec9340f82f9a9bb884fc5bb486ca513fe610797d
Ahmed Elkoussy <aaelkoussy@gmail.com>
2020-06-10 13:59:10 +0200

    added checkbox to narrow down to starred repos only

--- src/containers/ListOfRepos.jsx
+++ src/containers/ListOfRepos.jsx
@@ -1,40 +1,74 @@
 import React, { useEffect, useState } from "react";
 import { RepoItem } from "../components/RepoItem";
+import Checkbox from "@material-ui/core/Checkbox";
 import axios from "axios";
 
 export const ListOfRepos = () => {
-  const [arrayOfRepos, setArrayOfRepos] = useState([]);
+  const [allRepos, setAllRepos] = useState([]);
+  const [reposToShow, setReposToShow] = useState([]);
+  const [showStarredOnly, setShowStarredOnly] = useState(false);
+
   useEffect(() => {
+    const dateOfOneWeekAgoUTC = getOneWeekAgoDate();
+
+    // possible improvements here to keep all the params as variables and allow changing them for the user
+    // get the list of repos from GitHub api
+    const queryParamsForGitHub = `>${dateOfOneWeekAgoUTC}&sort=stars&order=desc`;
+    const github_url = `https://api.github.com/search/repositories?q=created:${queryParamsForGitHub}`;
+
+    axios.get(github_url).then((response) => {
+      const repos = response.data.items;
+
+      setReposToShow(repos);
+      setAllRepos(repos);
+    });
+  }, []);
+
+  const handleChange = (event) => {
+    const checked = event.target.checked;
+    setShowStarredOnly(checked);
+    var starredIDs = JSON.parse(localStorage.getItem("starred")) || [];
+
+    if (checked) {
+      const starredRepos = allRepos.filter((repo) =>
+        starredIDs.includes(repo.id)
+      );
+      setReposToShow(starredRepos);
+    } else {
+      setReposToShow(allRepos);
+    }
+  };
+
+  const getOneWeekAgoDate = () => {
     //Get today's date using the JavaScript Date object.
     var ourDate = new Date();
     //Change it so that it is 7 days in the past.
     var dateOfOneWeekAgo = ourDate.getDate() - 7;
     ourDate.setDate(dateOfOneWeekAgo);
     const dateOfOneWeekAgoUTC = ourDate.toISOString().split("T")[0];
-
-    // possible improvements here to keep all the params as variables and allow changing them for the user
-    // get the list of repos from GitHub api
-    const queryParamsForGitHub = `>${dateOfOneWeekAgoUTC}&sort=stars&order=desc`;
-    const github_url = `https://api.github.com/search/repositories?q=created:${queryParamsForGitHub}`;
-    axios.get(github_url).then((response) => {
-      const repos = response.data.items;
-      console.log(repos);
-      setArrayOfRepos(repos);
-    });
-
-    // retrieve the starred repos from localstorage
-  }, []);
+    return dateOfOneWeekAgoUTC;
+  };
 
   return (
     <div>
-      {arrayOfRepos.map((repo) => (
+      {/* TODO make this a label for the checkbox */}
+      <p>Show only starred repos</p>
+      <Checkbox
+        checked={showStarredOnly}
+        onChange={handleChange}
+        inputProps={{
+          "aria-label": "primary checkbox",
+          title: "show only starred repos",
+        }}
+      />
+      {reposToShow.map((repo) => (
         <RepoItem
           name={repo.name}
           githubLink={repo.html_url}
           description={repo.description}
           stars={repo.stargazers_count}
           key={repo.id}
-          objectId={repo.id}
+          objectID={repo.id}
         />
       ))}
     </div>
