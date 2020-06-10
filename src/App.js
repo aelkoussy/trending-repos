d0cbb3573908ed46db48b9a5233de8854e4f4189
Ahmed Elkoussy <aaelkoussy@gmail.com>
2020-06-10 02:45:14 +0200

    added css and cards for beautification

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import { ListOfRepos } from "./containers/ListOfRepos";
+import { CssBaseline } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <h2>List of most popular repositories for the last week</h2>
-      <ListOfRepos />
-    </div>
+    <CssBaseline>
+      <div className="App">
+        <h2>List of most popular repositories for the last week</h2>
+        <ListOfRepos />
+      </div>
+    </CssBaseline>
   );
 }
 
--- src/components/RepoItem.jsx
+++ src/components/RepoItem.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { StarBtn } from "../material-ui/StarBtn";
+import { Card } from "@material-ui/core";
 
 export const RepoItem = ({ name, githubLink, description, stars }) => {
   return (
-    <div>
-      <a href={githubLink}>
-        <h2>{name}</h2>
-      </a>
-      <p>{description}</p>
-      <h4>{stars} stars</h4>
-    </div>
+    <Card variant="outlined">
+      <div>
+        <a href={githubLink}>
+          <h2>{name}</h2>
+        </a>
+        <p>{description}</p>
+        <h4>{stars} stars</h4>
+      </div>
+    </Card>
   );
 };
